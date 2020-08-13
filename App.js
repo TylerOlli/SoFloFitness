e3bd4016716bf27cdd8d37f929b6ad3fb6577778
Tyler Olli <ollityler@gmail.com>
2020-08-12 23:03:14 -0400

    feat: Created initial AddEntry component

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
 
-export default function App() {
-  const [counter, setCounter] = React.useState(0);
-  console.log('Before');
-  debugger;
-  console.log('After');
-  return (
-    <View style={styles.container}>
-      <Text>{counter}</Text>
-      <Button
-        onPress={() => {
-          setCounter(counter + 1);
-        }}
-        title='Increment'
-      />
-      <Button
-        onPress={() => {
-          setCounter(counter - 1);
-        }}
-        title='Decrement'
-      />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
